48b6b85931079507ba421cbbbb4c94eea0f2803e
whutfaker <1312976124@qq.com>
2021-07-13 00:22:23 +0800

    feat: 迁移CDN到cli

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Index from '../views/index.vue'
+import MIndex from '../views/mIndex.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'index',
+    component: navigator.userAgent.match(/(iPhone|iPod|Android|ios|iPad)/i) ? MIndex : Index
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/m_index',
+    name: 'm_index',
+    component: () => import(/* webpackChunkName: "m_index" */ '../views/mIndex.vue')
+  },
+  {
+    path: '/maintaining',
+    name: 'maintaining',
+    component: () => import(/* webpackChunkName: "maintaining" */ '../views/maintaining.vue')
+  },
+  {
+    path: '/result',
+    name: 'result',
+    component: () => import(/* webpackChunkName: "result" */ '../views/result.vue')
+  },
+  {
+    path: '/protocol',
+    name: 'protocol',
+    component: () => import(/* webpackChunkName: "result" */ '../views/protocol.vue')
+  },
+  // 兼容旧版本.html
+  {
+    path: '/m_index.html',
+    name: 'm_index',
+    component: () => import(/* webpackChunkName: "m_index" */ '../views/mIndex.vue')
+  },
+  {
+    path: '/maintaining.html',
+    name: 'maintaining',
+    component: () => import(/* webpackChunkName: "maintaining" */ '../views/maintaining.vue')
+  },
+  {
+    path: '/result.html',
+    name: 'result',
+    component: () => import(/* webpackChunkName: "result" */ '../views/result.vue')
+  },
+  {
+    path: '/protocol.html',
+    name: 'protocol',
+    component: () => import(/* webpackChunkName: "result" */ '../views/protocol.vue')
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
