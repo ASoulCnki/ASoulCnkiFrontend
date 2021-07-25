8c26fd0a12284bf186926e8b1d9ca547da3fa328
NorthChick <ch3cknull@outlook.com>
2021-07-25 16:42:22 +0800

    pref: remove unused files
    1. remove unused picture
    2. remove maintianing.vue and path in route/index.js
    3. fix style in rank
    4. remove unused lib in element.js
    5. add new file maintaining.html

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Element from './config/element'
-import 'normalize.css/normalize.css'
 import "tailwindcss/tailwind.css"
 
 Vue.use(Element, {
