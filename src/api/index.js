0f8d2198ee0d90b17c2d7149dfbd0799e17283d4
ch3cknull <ch3cknull@outlook.com>
2021-08-13 17:36:22 +0800

    fix: fix symbol without encodeURI

--- src/api/index.js
+++ src/api/index.js
@@ -3,4 +3,19 @@ axios.defaults.timeout = 4000 // mill seconds
 const instance = axios.create({
     baseURL: process.env.VUE_APP_BASE_API || 'https://asoulcnki.asia/v1/api'
 })
+
+instance.interceptors.request.use(config => {
+    let url = config.url
+    if (config.method === 'get' && config.params) {
+        url += '?'
+        for (let key in config.params) {
+            url += `${key}=${encodeURIComponent(config.params[key])}&`
+        }
+        url = url.substring(0, url.length - 1)
+        config.params = {}
+    }
+    config.url = url
+    return config
+}
+)
 export { instance }
