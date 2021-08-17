f4b085169366511ce22f2c47ac8599979eaaa51e
jonathan.wang <jonathan_wang@ehsy.com>
2021-08-17 17:46:05 +0800

    fix: 修复第二页筛选重复请求，button样式调整

--- src/config/element.js
+++ src/config/element.js
@@ -3,7 +3,7 @@ import CarouselItem from 'element-ui/packages/carousel-item/index';
 import Carousel from 'element-ui/packages/carousel/index';
 import Dialog from 'element-ui/packages/dialog/index'
 import CheckBox from 'element-ui/packages/checkbox/index'
-import Input from'element-ui/packages/input/index';
+import Input from 'element-ui/packages/input/index';
 
 const element = {
   install: (Vue) => {
