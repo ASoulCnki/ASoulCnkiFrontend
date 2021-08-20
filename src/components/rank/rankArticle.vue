30f517888757f8448332aacdc9760b3667c4cfeb
ch3cknull <ch3cknull@outlook.com>
2021-08-20 12:09:19 +0800

    style: 修复rank样式，添加友链

--- src/components/rank/rankArticle.vue
+++ src/components/rank/rankArticle.vue
@@ -1,140 +1,91 @@
 <template>
-  <div class="article-item">
-    <div class="item-head p-2">
-      <span class="py-3">
-        <span class="iconfont icon-yonghu"></span>
-        <a
-          :href="'https://space.bilibili.com/' + article.author.id"
-          class="url" target="_blank"
-          :title="article.author.name"
-        >
-          <span class="sm:hidden">{{ authorName }}</span>
+  <div class="article">
+    <div class="article-header">
+      <div class="user inline">
+        <span class="iconfont icon-yonghu mr-1"></span>
+        <a :href="'https://space.bilibili.com/' + article.author.id" target="_blank" class="hover">
           <span class="hidden sm:inline">{{ article.author.name }}</span>
+          <span class="sm:hidden">{{ article.author.name.substr(0,4) + '...' }}</span>
         </a>
-      <span
-        class="py-3 text-red-500 font-bold dark:text-yellow-500"
-        title="鉴定为[原创/原偷],请根据情况自行判断是否属实"
-        v-if="article.originReplyId < 0"
-      >
-        [原创/原偷]
-      </span>
-      </span>
-      <span class="float-right">
-        <a :href="article.url" target="_blank" class="url">
-          <span class="iconfont icon-lianjie">传送门</span>
-        </a>
-      </span>
-      <span 
-        title="一键偷走" class="copy" 
-        :id="'copy' + article.replyId + article.id"
-      >
-        复制
-      </span>
+        <span class="text-green-500 px-2" v-if="article.originReplyId == -1">[原创|原偷]</span>
+      </div>
+      <div class="right-button text-blue-500 hover">
+        <a :href="article.url" target="_blank"><span class="iconfont icon-lianjie" />链接</a>
+      </div>
+      <div class="right-button hover pr-2 copy" @click="copy">
+        <span class="iconfont icon-fuzhi" />复制
+      </div>
     </div>
-    <!-- <div class="article-content" v-if="markedText" v-html="content"></div>
-    <div class="article-content" v-else>{{ article.content }}</div> -->
-    <div class="article-content">{{ article.content }}</div>
-
+    <div class="article-text">{{ article.content }}</div>
     <div class="article-footer">
-      <span class="mt-1" title="累计点赞数">
-        <span class="iconfont icon-dianzan_active"></span>
-        {{article.totalLikeSum}}
-      </span>
-      <span class="mt-1" title="点赞数">
-        <span class="iconfont icon-dianzan"></span>
-        {{article.likeNum}}
-      </span>
-      <span class="mt-1" title="引用数">
-        <span class="iconfont icon-yinyong"></span>
-        {{article.similarCount}}
-      </span>
-      <span class="mt-1" title="发布时间">
-        <span class="iconfont icon-shijian"></span>
-        {{ article.createTime }}
-      </span>
+      <div class="footer-attr">
+        <span class="iconfont icon-dianzan_active" />{{ article.totalLikeSum }}
+      </div>
+      <div class="footer-attr">
+        <span class="iconfont icon-dianzan" />{{ article.likeNum }}
+      </div>
+      <div class="footer-attr">
+        <span class="iconfont icon-yinyong" />{{ article.similarCount }}
+      </div>
+      <div class="footer-attr">
+        <span class="iconfont icon-shijian" />{{ article.createTime }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import clipboard from 'clipboard'
+
 export default {
   name: 'RankArticle',
   props: {
     article: Object,
-    markArray: {
-      default: () => []
-    },
-  },
-  data() {
-    return {
-      clipboard: null
-    }
   },
   methods: {
-    notify(s, type) {
-      this.$message({showClose: true, message: s, type: type})
-    }
-  },
-  computed: {
-    authorName() {
-      const name = this.article.author.name
-      return name.length <= 6 ? name : name.substr(0, 5) + '...'
+    notify(message, type="success") {
+      this.$message({showClose: true, message, type})
     },
-    markedText() {
-      return this.markArray.value.length > 0 
-      // return this.marked && this.markArray.length > 0 
-    },
-    content() {
-      let content = this.article.content;
-      const arr = this.markArray.value.split(',').sort((a, b) => b.length - a.length)
-      for (const regex of arr) {
-        content = content.replaceAll(regex, s => `<span class="marked">${s}</span>`)
-      }
-      return content
+    copy() {
+      const clip = new clipboard('.copy', { text: () => this.article.content })
+      .on('success', () => {
+        this.notify('复制成功，适度玩梗捏')
+        clip.destroy()
+      })
+      .on('error', () => message('复制失败', 'error') )
     }
-  },
-  mounted() {
-    let clipboard = new ClipboardJS('#copy' + this.article.replyId + this.article.id, {
-      text: () => this.article.content
-    })
-      .on("success", () => {this.notify('复制成功,适度玩梗捏', 'success')})
-      .on("error", () => this.notify('复制失败，请手动复制', 'warning'))
-    this.clipboard = clipboard
-  },
-  beforeDestroy() {
-    this.clipboard.listener.destroy()
   }
 }
 </script>
 
 <style scoped>
-.article-item {
-  @apply w-full p-2 my-4 bg-gray-100 rounded-md overflow-hidden;
-  @apply dark:bg-gray-700 dark:text-gray-300;
+.article {
+  @apply w-full p-2 mx-auto text-lg mt-3 mb-4 border border-gray-200 dark:border-gray-800;
+  @apply bg-gray-100 text-gray-600 rounded-md shadow-md;
+  @apply dark:text-gray-400 dark:bg-gray-700; 
 }
 
-.url {
- @apply text-blue-500 dark:text-blue-300;
+.article-header { @apply p-1 pb-2 divide-x text-2xl; }
+
+.right-button { @apply inline float-right pl-2 cursor-pointer; }
+
+.hover {
+  @apply cursor-pointer hover:text-blue-400 dark:hover:text-yellow-500;
 }
 
-.copy {
-  @apply float-right border px-5 mr-2 rounded-md bg-blue-400 text-white cursor-pointer;
-  @apply dark:bg-gray-500 dark:text-gray-200;
-  @apply hover:bg-blue-500 transition;
-  margin-top: -1px
+.article-text {
+  @apply p-2 rounded-md;
+  @apply text-2xl leading-relaxed break-all whitespace-pre-wrap;
+  @apply bg-gray-200 dark:bg-gray-800 dark:text-gray-400;
 }
 
-.article-content {
-  @apply rounded-md p-4 bg-gray-200;
-  @apply dark:bg-gray-800 dark:text-gray-300;
-  @apply leading-relaxed whitespace-pre-wrap overflow-hidden;
+.article-footer { @apply grid grid-cols-2 md:grid-cols-4 pt-2; }
+
+.footer-attr {
+  @apply text-left text-2xl font-mono pl-1;
+  @apply dark:hover:text-gray-300 hover:text-gray-800;
 }
 
-.article-footer {
-  @apply text-2xl pt-2 px-2 grid grid-cols-2 lg:grid-cols-4 w-full;
-}
+.footer-attr .iconfont { @apply mx-2; }
 
-.iconfont {
-  @apply mx-2;
-}
 </style>
